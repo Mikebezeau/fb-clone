a6a597e9f17eb0a4156a9ad1e994214ba57baca1
Mikebezeau <mikebezeau@gmail.com>
2022-06-10 13:44:42 -0400

    Change Project Name
    Changed project name, and fixed minor issues with images.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
-import React from 'react';
-import './App.css';
-import Login from './components/Login';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import Feed from './components/Feed';
-import Widget from './components/Widget';
-import { useStateValue } from './state/StateProvider';
+import React from "react";
+import "./App.css";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import Widget from "./components/Widget";
+import { useStateValue } from "./state/StateProvider";
+
+/* FIREBASE DATA RULES
+
+rules_version = '2';
+service firebase.storage {
+  match /b/{bucket}/o {
+    match /{allPaths=**} {
+      allow read, write: if request.auth != null;
+    }
+  }
+}
+
+*/
 
 function App() {
-
   const [{ user }, dispatch] = useStateValue();
 
   return (
     <div className="app">
-      { !user ? (
-          <Login/>
-        ) : (
-          <>
-            <Header/>
-            <div className="app_body">
-              <Sidebar/>
-              <Feed/>
-              <Widget/>
-            </div>
-          </>
-        )
-      }
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
+          <div className="app_body">
+            <Sidebar />
+            <Feed />
+            <Widget />
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-import firebase from 'firebase';
+import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBaDdBDPfGi6i0bezyj2hIBFNbHMdfbjEo",
-    authDomain: "facebook-clone-22f4e.firebaseapp.com",
-    projectId: "facebook-clone-22f4e",
-    storageBucket: "facebook-clone-22f4e.appspot.com",
-    messagingSenderId: "19077083986",
-    appId: "1:19077083986:web:cb1ea59d6da96b6cd2408b",
-    measurementId: "G-WT05NSNJBM"
-  };
+  apiKey: "AIzaSyDCXv8tyGx2aBFbCYjPJf_bqgsOBOnxwoE",
+  authDomain: "chat-49118.firebaseapp.com",
+  projectId: "chat-49118",
+  storageBucket: "chat-49118.appspot.com",
+  messagingSenderId: "776423265723",
+  appId: "1:776423265723:web:5a4db24d8014768af4e09e",
+  measurementId: "G-963J6DXJ5T",
+};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
